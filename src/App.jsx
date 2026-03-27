392173decdb8521ab9b7b5cb0b1f079e341bd341
Harshith9477 <harshith.donthineni.s.145@kalvium.community>
2026-03-27 14:52:42 +0530

    feat: add country detail page with routing and custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
 import Home from "./pages/Home";
+import CountryPage from "./pages/CountryPage";
+import NotFound from "./pages/NotFound";
+
+// Components
+import Header from "./components/Header";
+
+// CSS
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <h1 style={{ padding: "20px" }}>CountryPeek</h1>
+    <Router>
+      <Header />
 
       <Routes>
+        {/* Home Page */}
         <Route path="/" element={<Home />} />
+
+        {/* Country Detail Page (PART 3) */}
+        <Route path="/country/:code" element={<CountryPage />} />
+
+        {/* 404 Page */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,23 +1,31 @@
 import { Link } from "react-router-dom";
 
 function CountryCard({ country }) {
-  const { name, flags, population, region, capital, cca3 } = country;
-
   return (
-    <Link to={`/country/${cca3}`} className="card">
+    <Link to={`/country/${country.cca3}`} className="card">
+      
       <img
-        src={flags?.png}
-        alt={name?.common}
+        src={country.flags.svg}
+        alt={country.name.common}
         className="card__flag"
       />
 
       <div className="card__body">
-        <h3 className="card__name">{name?.common || "No Name"}</h3>
+        <h3 className="card__name">{country.name.common}</h3>
 
-        <p><b>Population:</b> {population?.toLocaleString() || "N/A"}</p>
-        <p><b>Region:</b> {region || "N/A"}</p>
-        <p><b>Capital:</b> {capital?.[0] || "N/A"}</p>
+        <p>
+          <b>Population:</b> {country.population.toLocaleString()}
+        </p>
+
+        <p>
+          <b>Region:</b> {country.region}
+        </p>
+
+        <p>
+          <b>Capital:</b> {country.capital?.[0] || "N/A"}
+        </p>
       </div>
+
     </Link>
   );
 }
